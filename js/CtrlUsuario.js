6a3ddbd12dfbcb26628de9aa2650f1611106d337
salf3cv51 <salf3cv51@gmail.com>
2021-12-16 14:18:08 -0600

    everything 1.11

--- js/CtrlUsuario.js
+++ js/CtrlUsuario.js
@@ -58,7 +58,9 @@ async function busca() {
           import("./tipos.js").
                   Jugador} */
       const data = doc.data();
-      
+      const formData =
+      new FormData(forma);
+      const correo =  getString(formData, "correo").trim();
       img.src = await urlStorage(correo);
       forma.nombre.value = data.nombre|| "";
       forma.fechaNacim.value = data.fechaNacim|| "";
