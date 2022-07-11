0d0a8c22648b73073b6eeafdf60b82539480021b
salf3cv51 <95593912+salf3cv51@users.noreply.github.com>
2022-07-11 02:46:21 -0500

    solo pedidos

--- js/navegacion.js
+++ js/navegacion.js
@@ -1,30 +1,5 @@
 export function
-  muestraEquipos() {
+  muestraPedidos() {
   location.href =
     "index.html";
 }
-
-export function
-  muestraJugadores() {
-  location.href = "jugadores.html";
-}
-export function
-  muestraSanciones() {
-  location.href = "sanciones.html";
-}
-
-export function
-  muestraAvisos() {
-  location.href = "avisos.html";
-}
-
-export function
-  muestraArbitros() {
-  location.href = "arbitros.html";
-}
-
-export function
-  muestraDelegados() {
-  location.href = "delegados.html";
-}
-
