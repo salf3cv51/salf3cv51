2f73dedce4e4ad750126994787e69c24f31e1806
salf3cv51 <95593912+salf3cv51@users.noreply.github.com>
2022-07-11 02:33:15 -0500

    Delete CtrlEquiposDelegado.js

--- js/CtrlEquiposDelegado.js
+++
@@ -1,6 +0,0 @@
-// Create a reference to the cities collection
-import { collection, query, where } from "firebase/firestore";
-const citiesRef = collection(db, "cities");
-
-// Create a query against the collection.
-const q = query(citiesRef, where("state", "==", "CA"));
