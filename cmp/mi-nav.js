1cf88f9515b3878b3724b1485702a0b413bc3bfb
salf3cv51 <95593912+salf3cv51@users.noreply.github.com>
2023-06-06 12:54:54 -0600

    Add files via upload
    navbar

---
+++ cmp/mi-nav.js
@@ -0,0 +1,98 @@
+// @ts-nocheck
+import {
+  cargaRoles
+} from "../js/seguridad.js";
+import {
+  getAuth
+} from "../lib/fabrica.js";
+import {
+  muestraError
+} from "../lib/util.js";
+
+class MiNav extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `<ul>
+        <li>
+          <a href="index.html">
+            Sesión</a>
+        </li>
+      </ul>`;
+    this.ul =
+      this.querySelector("ul");
+    getAuth().onAuthStateChanged(
+      usuario => this.
+        cambiaUsuario(usuario),
+      muestraError);
+  }
+
+  /**
+   * @param {import(
+      "../lib/tiposFire.js").User}
+      usu */
+      async cambiaUsuario(usu) {
+        if (usu && usu.email) {
+          let html = "";
+          const roles =
+            await cargaRoles(
+              usu.email);
+          /* Enlaces para solo
+           * para clientes. */
+          if (roles.has("Cliente")) {
+            html += /* html */
+              `<li>
+                <a href=
+                  "chat.html">Chat</a>
+              </li>`;
+          }
+          if (roles.has("Jugador")) {
+            html += /* html */
+            `<li>
+              <a href="equiposJugador.html">Mis Equipos</a>
+            </li>`;
+          }
+          if (roles.has("Delegado")) {
+            html += /* html */
+            `<li>
+              <a href="equiposDelegado.html">Mis Equipos</a>
+            </li>`;
+          }
+          if (roles.has("Arbitro")) {
+            html += /* html */
+            `<li>
+              <a href="sanciones.html">Sanciones</a>
+            </li>`;
+          }
+          /* Enlaces para solo
+           * administradores.
+           */
+          if (roles.has(
+            "Administrador")) {
+            html += /* html */
+              `<li>
+                <a href="equipos.html">Equipos</a>
+              </li>
+              <li>
+                <a href="jugadores.html">Jugadores</a>
+              </li>
+              <li>
+              <a href="delegados.html">Delegados</a>
+            </li>
+            <li>
+              <a href="arbitros.html">Arbitros</a>
+            </li>
+          <li>
+            <a href="sanciones.html">Sanciones</a>
+          </li>
+              <li>
+                <a href="avisos.html">Avisos</a>
+              </li>`;
+              
+          }
+          this.ul.innerHTML += html;
+        }
+      }
+    }
+
+
+customElements.define("mi-nav", MiNav);
